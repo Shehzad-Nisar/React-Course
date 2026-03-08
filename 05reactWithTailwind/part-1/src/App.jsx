2740b0196877e03379a0697728c8dfaac534d81a
Shehzad-Nisar <shehzadnisar07@gmail.com>
2026-03-08 06:43:34 +0500

    new commits

--- 05reactWithTailwind/part-1/src/App.jsx
+++ 05reactWithTailwind/part-1/src/App.jsx
@@ -1,23 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
+    <div className="relative w-full h-screen overflow-hidden">
+      {/* Background Image */}
       <div
-      className="min-h-screen w-full bg-amber-300 bg-cover bg-center bg-no-repeat flex items-center justify-center"
-      style={{ backgroundImage: "url('./public/bgImg.jpg')" }}
-    >
-      <h1 className="text-white text-4xl p-10">
-        Background Image
-      </h1>
-    </div>
-    </>
-  )
-}
+        className="opacity-90  absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: `url('/bgImg.jpg')` }}
+      ></div>
 
-export default App
+      {/* Overlay Content */}
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4">
+        <h1 className="text-4xl font-sans text-shadow-lg/30  md:text-6xl font-bold mb-2">
+         Unlimited movies,  TV <br /> shows, and more
+        </h1>
+        <p className="text-2xl mt-0 md:text-2xl mb-6 text-shadow-lg/30 ">
+          <b>Starts at Rs 250. Cancel anytime.</b>
+        </p>
+         <p className=" text-xl mb-3 text-shadow-lg/30  ">
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+        
+        
+        <button onClick={()=>alert("your have started!!")} style={{ backgroundColor: '#E50914' }} className="  text-white font-bold py-3 px-6 rounded  ">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+}
