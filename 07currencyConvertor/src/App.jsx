fb0f576a0fe3d689dfd6948e5aa7ffab297cc169
Shehzad-Nisar <shehzadnisar07@gmail.com>
2026-03-14 04:05:58 +0500

    updated

--- 07currencyConvertor/src/App.jsx
+++ 07currencyConvertor/src/App.jsx
@@ -4,15 +4,46 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import {InputBox} from './components/index.js'
+import useCurrencyDetails from './Hooks/useCurrencyDetails.js'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [amount , setAmount] = useState(100)
+  const [convertedAmount , setConvertedAmount]= useState(0)
+  const [from , setFrom ] = useState("usd")
+  const [to , setTo] = useState('pkr')
+  const fetchRates = useCurrencyDetails(from)
+  const options = Object.keys(fetchRates);
+  
+  
+  //swapping function which just swaps values on pressing button 
+  function swap(){
+    setFrom(to);
+    setTo(from);
+    setAmount(convertedAmount);
+    setConvertedAmount(amount);
+  }
+
+  //function for conversion :
+
+  const convert = ()=>{
+    setConvertedAmount(amount * fetchRates[to])
+    
+
+  }
+  
+  
+
+
+  
+  
+
+  
 
   return (
     <>
     
-      <h1 className='bg-red-500 text-4xl ' >Currency convertor</h1>
-      <InputBox/>
+      
     </>
   )
 }
--- 07currencyConvertor/src/Hooks/useCurrencyDetails.js
+++ 07currencyConvertor/src/Hooks/useCurrencyDetails.js
@@ -6,11 +6,13 @@ function useCurrencyDetails(currency){
     const [data, setData]= useState({})
 
    useEffect( ()=>{
-    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
     .then((response)=>response.json())
     .then((response)=> setData(response[currency]))
 
    } , [currency])
+
+   return data;
 }
 
 export default useCurrencyDetails;
