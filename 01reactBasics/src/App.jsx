83cf4cd590c3fcd467f7d66078de2d65df299e14
Shehzad-Nisar <shehzadnisar07@gmail.com>
2026-03-15 07:18:03 +0500

    practices

--- 01reactBasics/src/App.jsx
+++ 01reactBasics/src/App.jsx
@@ -1,22 +1,38 @@
-import React, { useState } from "react";
-import Button from './component/Button.jsx'
+import React, { useState } from 'react'
+import useCurrencyInfo from './Hook/useCurrencyInfo'
 
-function App() {
-  const [count, setCount] = useState(0);
 
-  // Function recreated on every render
-  const increment = () => {
-    console.log("Increment clicked");
-    setCount(prev => prev + 1);
-  };
+const App = () => {
+
+  const [amount , setAmount] = useState(0);
+
+  
+  
+
+
+  
+
 
   return (
-    <div>
-      <h3>Count: {count}</h3>
-      <Button onClick={increment} />
-      <button onClick={() => setCount(count + 10)}>Parent Update</button>
-    </div>
-  );
+    <>
+     <div className=''>
+      <input type="number"
+       value={amount}
+       placeholder='amount'
+       onChange={(e)=> setAmount(e.target.value)}
+      
+      />
+     </div>
+
+
+
+    
+
+    
+    
+    
+    </>
+  )
 }
 
-export default App;
+export default App
