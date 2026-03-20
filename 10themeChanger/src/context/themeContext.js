58aea80405be2b0a1d85f014a84bb030b6de83a1
Shehzad-Nisar <shehzadnisar07@gmail.com>
2026-03-20 07:41:36 +0500

    new commits

---
+++ 10themeChanger/src/context/themeContext.js
@@ -0,0 +1,15 @@
+import React, { createContext, useContext } from "react";
+
+export const  ThemeContext = createContext({
+    themeMode : 'light', 
+    lightMode : ()=>{},
+    darkMode : ()=>{}
+})
+
+
+export const  ThemeProvider = ThemeContext.Provider;
+
+export function useTheme(){
+    return useContext(ThemeContext);
+
+}
