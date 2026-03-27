fd40c9e83d8c3498f5675898d7c800b445223f1a
Shehzad-Nisar <shehzadnisar07@gmail.com>
2026-03-27 11:09:22 +0500

    features done

---
+++ 13todoUsingRedux/src/features/todo/todoSlice.js
@@ -0,0 +1,34 @@
+import { createSlice,nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [
+        {
+            id: nanoid(),
+            title: "first task!",
+            completed: false
+        }
+    ]
+}
+
+export const todoSlice = createSlice({
+    name: 'todos',
+    initialState,
+    reducers: {
+        addTodo: (state,action)=>{
+            const todo = {
+                id : nanoid(),
+                title: action.payload,
+                completed: false
+
+            }
+            state.todos.push(todo)
+
+        },
+        deleteTodo: (state,action) => {
+           state.todos = state.todos.filter((todo)=> todo.id!= action.payload)
+        },
+        updateTodo: (state,action)=>{
+            state.todos = state.todos.map((eachTodo)=> eachTodo.id=== action.payload? eachTodo.title=action.title.payload:eachTodo)
+        }
+    }
+})
